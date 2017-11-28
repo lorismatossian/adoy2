f67c545e95f403e30aa395fc8a07084c8eb81c83
Loris Matossian <lorismatossian@gmail.com>
2017-11-28 15:58:35 +0100

    config mongodb

--- fortune2/testExpress/myapp/config/database.js
+++ fortune2/testExpress/myapp/config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
 
-    'url' : 'mongodb://127.1.1.0/express' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'http://127.1.1.0/27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
      
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+
+     
 };

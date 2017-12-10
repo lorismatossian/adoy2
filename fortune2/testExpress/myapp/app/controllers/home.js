349b3bf088edd27a31bbc107b1d2ea8cd1a02f98
Daniel Ngahan <ngahan_d@epita.fr>
2017-12-10 21:26:59 +0100

    [controller] - add create,delete,vote methode to home controller Fonction non testée

--- fortune2/testExpress/myapp/app/controllers/home.js
+++ fortune2/testExpress/myapp/app/controllers/home.js
@@ -1,7 +1,63 @@
 var numeral = require('numeral');
 var bcrypt = require('bcrypt-nodejs');
 var dateFormat = require('dateformat');
-const User = require('../models/user');
+const Fortune = require('../models/fortune');
+const User = require('../models/home');
+
+
+
+exports.index = function(req, res)
+{
+	fortunes = fortune.findAll();
+	res.render("home/index");
+};
+
+exports.create_fortune = function(req, res)
+{
+	const fort = new fortune(req.body);
+	fort.title = req.title;
+	fort.content = req.content;
+	if (req.user.id != null) {
+		fort.user_id = req.user.id;
+	}
+	else {
+		fort.user_id = null;
+	}
+	fort.score = 0;
+	fort.save();
+	if (err) {
+		res.render("/error", {error: err});
+	}
+	else {
+		res.redirect("/");
+	}
+};
+
+exports.delete_fortune = function(req, res)
+{
+	fort = req.fortune;
+	fort.remove();
+	if (err) {
+		res.render("/error", {error: err});
+	}
+	else {
+		res.redirect("/");
+	}
+};
+
+exports.vote = function(req, res)
+{
+	fort = req.fortune;
+	fort.score = fort.score + req.body;
+	fort.save();
+	if (err) {
+		res.render("/error", {error: err});
+	}
+	else {
+		res.redirect("/");
+	}
+};
+
 
 exports.loggedIn = function(req, res, next)
 {

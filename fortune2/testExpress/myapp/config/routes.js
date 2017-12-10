300176946e13565ed993d02aa6f913e3a6ed800a
Daniel Ngahan <ngahan_d@epita.fr>
2017-12-11 00:26:19 +0100

    [route] - change the home page of the application to index and the add page

--- fortune2/testExpress/myapp/config/routes.js
+++ fortune2/testExpress/myapp/config/routes.js
@@ -1,16 +1,22 @@
 var home = require('../app/controllers/home');
+var fortune = require('../app/models/fortune');
 
 //you can include all your controllers
 
 module.exports = function (app, passport) {
 
+
+    app.get('/', home.index);
+
     app.get('/login', home.login);
     app.get('/signup', home.signup);
+
     app.get('/add', home.add);
+    app.post('/add', home.create_fortune);
 
 
 
-    app.get('/', home.loggedIn, home.home);//home
+    //app.get('/', home.loggedIn, home.home);//home
     app.get('/home', home.loggedIn, home.home);//home
 
     app.post('/signup', passport.authenticate('local-signup', {
@@ -25,11 +31,11 @@ module.exports = function (app, passport) {
         failureFlash: true // allow flash messages
     }));
 
-    app.post('/add', passport.authenticate('local-login', {
+    /*app.post('/add', passport.authenticate('local-login', {
         successRedirect: '/home',
         failureRedirect: '/login',
         failureFlash: true
 
 
-    }));
+    }));*/
 }
